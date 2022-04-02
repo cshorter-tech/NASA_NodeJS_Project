11a11b468a7827e0f7d1bc384fdf24042c7e68cb
cshorter-tech <cshorter.tech@gmail.com>
2022-04-01 20:30:16 -0400

    fixed the drop down; needed to add the API URLs to request.js again

--- server/src/routes/launches/launch.controller.js
+++ server/src/routes/launches/launch.controller.js
@@ -3,7 +3,7 @@ const {launches} = require("../../models/launch.model");
 // Connect launches to values via iterator
 
 function getAllLaunches(req, res) {
-  return res.status(200).json(Arrary.from(launches.values()));
+  return res.status(200).json(Array.from(launches.values()));
 }
 
 module.exports = {
--- server/src/routes/launches/launch.router.js
+++ server/src/routes/launches/launch.router.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const {getAllLaunches} = require("./launch.controller");
+
 const launchesRouter = express.Router();
 
 launchesRouter.get("/launches", getAllLaunches);
